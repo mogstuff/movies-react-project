701e4c1177daa084f2d8afeaac1b799a06df9412
mogstuff <markmorgan.mcp@gmail.com>
2021-05-14 10:19:34 +0100

    refactor: boilerplate html updated

--- src/App.js
+++ src/App.js
@@ -3,7 +3,16 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      
+        <header><h1>Movies App</h1></header>
+      <main>
+       <section id="intro">
+         <p>Search for Movies, Series and Episodes</p>
+       </section>
+       <section id="searchMoviesForm">
+
+       </section>
+      </main>
+    <footer>&copy; Mark Morgan 2021</footer>
     </div>
   );
 }
